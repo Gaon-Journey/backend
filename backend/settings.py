fd9c2a07fb8564ba54d1cb39efd8819829fc4abc
Vishesh <visheshgarg570@gmail.com>
2024-11-13 16:55:16 +0530

    connected postgres

--- backend/settings.py
+++ backend/settings.py
@@ -87,9 +87,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv("POSTGRESQL_PASSWD"),
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
