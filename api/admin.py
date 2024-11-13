dcce63202d59d7e53d993730c9df97ef60434f3d
Vishesh <visheshgarg570@gmail.com>
2024-11-13 18:04:48 +0530

    added models to admin

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+import models
+
+
+admin.site.register(models.User)
+admin.site.register(models.Trip)
+admin.site.register(models.TripExtra)
+admin.site.register(models.BillingAddress)
+admin.site.register(models.BookedTrips)
